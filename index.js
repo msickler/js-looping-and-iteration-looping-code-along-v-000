95a5f1d88c86cbac78ace673b379878f97a480fb
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-18 22:53:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 // Code your solutions in this file
 const names = []
 
-function printBadges(names)
+function printBadges(names) {
+  for (let i = 0; i < names.length; i++) {
+    console.log(`Welcome ${name[i]}! You are employee #${i}.`)
+  }
+  
+}
